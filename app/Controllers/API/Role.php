56f1f6e452ab991e6ff151d8b8620c590ad18c17
Alejandro González Reyes <alexglzrys@gmail.com>
2021-07-17 11:37:49 -0500

    Controlador Role - registrar

--- app/Controllers/API/Role.php
+++ app/Controllers/API/Role.php
@@ -11,9 +11,25 @@ class Role extends ResourceController
 	{
 		$this->model = $this->setModel(new RoleModel());
 	}
+	
 	public function index()
 	{
 		$roles = $this->model->findAll();
 		return $this->respond($roles);
+	}
+
+	public function create()
+	{
+		try {
+			$role = $this->request->getJSON();
+			if ($this->model->insert($role)):
+				$role->id = $this->model->insertID();
+				return $this->respondCreated($role);
+			else:
+				return $this->failValidationError($this->model->validation->listErrors());
+			endif;
+		} catch (\Exception $e) {
+			return $this->failServerError($e->getMessage());
+		}
 	}
 }
