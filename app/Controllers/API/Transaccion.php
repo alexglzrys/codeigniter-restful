e153b08d6226d51ac88b4de3df9b6685c56b64e3
Alejandro González Reyes <alexglzrys@gmail.com>
2021-07-16 19:22:38 -0500

    Controlador transaccion - crear

--- app/Controllers/API/Transaccion.php
+++ app/Controllers/API/Transaccion.php
@@ -17,4 +17,19 @@ class Transaccion extends ResourceController
 		$transacciones = $this->model->findAll();
 		return $this->respond($transacciones);
 	}
+
+	public function create() 
+	{
+		try {
+			$transaccion = $this->request->getJSON();
+			if ($this->model->insert($transaccion)):
+				$transaccion->id = $this->model->insertID();
+				return $this->respondCreated($transaccion);
+			else:
+				return $this->failValidationError($this->model->validation->listErrors());
+			endif; 
+		} catch (\Exception $e) {
+			return $this->failServerError($e->getMessage());
+		}
+	}
 }
